f2f29771cf017753069330b67c75fdcbe5d4d54f
fgebhart <fabian.gebhart@gmail.com>
2016-06-26 10:30:56 +0200

    updated GUI script

--- code/tk_ao.py
+++ code/tk_ao.py
@@ -11,6 +11,7 @@ __author__ = 'Fabian Gebhart'
 # import the necessary packages
 import Tkinter as tk
 import os
+import time
 
 
 def delete_log():
@@ -23,8 +24,8 @@ def delete_log():
     the programs stop."""
     if os.path.isfile('/home/pi/close.log') is True:
         os.remove('/home/pi/close.log')
-    else:
-        pass
+
+delete_log()
 
 # initialize the total variable for the hidden buttons
 total = []
@@ -37,11 +38,29 @@ class Mainpage:
         master.geometry("800x480+0+0")
         # set background image:
         background_image = tk.PhotoImage(file=
-                    "/home/pi/background.gif")
+                    "/home/pi/background2.gif")
         background_label = tk.Label(master,
                     image=background_image)
         background_label.place(x=-1, y=-1)
         background_label.image = background_image
+        background_label.image = background_image
+
+
+        # manage image for the hidden button, which is the same
+        # as the background color of the main background image
+        hidden_button_background_image = tk.PhotoImage(file=
+                    "/home/pi/hidden_button_big.gif")
+
+        # set images for status label
+        status_image_on = tk.PhotoImage(file=
+                    "/home/pi/aktiv.gif")
+        status_image_off = tk.PhotoImage(file=
+                    "/home/pi/deaktiviert.gif")
+        status_image_camview = tk.PhotoImage(file=
+                    "/home/pi/Sicht_der_Kamera_aktiv.gif")
+        status_image_reset = tk.PhotoImage(file=
+                    "/home/pi/status_reset.gif")
+
 
         # set label to fullscreen
         master.attributes('-fullscreen', True)
@@ -51,47 +70,45 @@ class Mainpage:
             newwindow = tk.Toplevel()
             app = Instructions(newwindow)
 
-        # manage buttons:
-        button1 = tk.Button(text='Anleitung', width=25,
-                    height=2, command=lambda: new_window())
-        button1.place(relx=0.8, y=180, anchor="c")
-
         def start_ao():
             """function to run, when 'start'-button is pressed.
             Creates the 'stop'-button and runs the
             'AO_just_stabi.py' file"""
-            os.system('sudo python AO_just_stabi.py &')
-            button7.place(relx=0.25, y=260, anchor="c")
+            if os.path.isfile('/home/pi/close.log') is False:
+                os.system('sudo python AO_just_stabi.py &')
+                time.sleep(1)
+                button7.place(x=61, y=390, anchor="sw")
+                status_label_off.place_forget()
+                status_label_on.place(x=74, y=180)
 
-        # manage images for start and stop button
-        start_button_image = tk.PhotoImage(file=
-                    "/home/pi/start_button.gif")
-        stop_button_image = tk.PhotoImage(file=
-                    "/home/pi/stop_button.gif")
 
-        button2 = tk.Button(text='Start Adaptive Optik',
-                    width=275, height=275,
-                    highlightthickness=0, bd=0,
-                    command=lambda: start_ao(),
-                    image=start_button_image)
-        button2.image = start_button_image
-        button2.place(relx=0.25, y=260, anchor="c")
+        def reset():
+            if os.path.isfile('/home/pi/close.log') is False:
+                button7.place(x=61, y=390, anchor="sw")
+                status_label_off.place_forget()
+                status_label_reset.place(x=74, y=180)
+                os.system('sudo python AO_reset.py &')
+                # time.sleep(2)
 
-        button3 = tk.Button(text='Reset', width=25, height=2,
-                    command=lambda:
-                    os.system('sudo python AO_reset.py &'))
-        button3.place(relx=0.8, y=260, anchor="c")
+                while os.path.isfile('/home/pi/close.log') is True:
+                    time.sleep(1)
+                else:
+                    status_label_reset.place_forget()
+                    status_label_off.place(x=74, y=180)
+                    button7.place_forget()
+
 
         def cam_view():
             """function to run, when 'cam-view'-button is
             pressed. Creates the 'stop'-button and runs
             the 'just_cam.py' file"""
-            os.system('sudo python just_cam.py &')
-            button7.place(relx=0.25, y=260, anchor="c")
+            if os.path.isfile('/home/pi/close.log') is False:
+                os.system('sudo python just_cam.py &')
+                time.sleep(1)
+                button7.place(x=61, y=390, anchor="sw")
+                status_label_camview.place(x=74, y=180)
+                status_label_off.place_forget()
 
-        button4 = tk.Button(text='Kamera Sicht', width=25,
-                    height=2, command=lambda: cam_view())
-        button4.place(relx=0.8, y=340, anchor="c")
 
         # manage the hidden buttons + function:
         def hidden_button(count):
@@ -120,51 +137,93 @@ class Mainpage:
             else:
                 total = []
 
-        # manage image for the hidden button, which is the same
-        # as the background color of the main background image
-        hidden_button_background_image = tk.PhotoImage(file=
-                    "/home/pi/hidden_button.gif")
-
-        # 3 hidden buttons (5, 8, 9):
-        button5 = tk.Button(text ='', width=78, height=38,
-                    highlightthickness=0, bd=0,
-                    command=lambda: hidden_button(1),
-                    image=hidden_button_background_image)
-        button5.image = hidden_button_background_image
-        button5.place(relx=1, y=20, anchor="c")
-
-        button8 = tk.Button(text='', width=78, height=38,
-                    highlightthickness=0, bd=0,
-                    command=lambda: hidden_button(2),
-                    image=hidden_button_background_image)
-        button8.image = hidden_button_background_image
-        button8.place(relx=1, y=460, anchor="c")
-
-        button9 = tk.Button(text='', width=78, height=38,
-                    highlightthickness=0, bd=0,
-                    command=lambda: hidden_button(3),
-                    image=hidden_button_background_image)
-        button9.image = hidden_button_background_image
-        button9.place(relx=0, y=460, anchor="c")
-
         def stop_button():
             """function to run, when 'stop'-button is pressed.
             Creates the 'stop'-button and runs the
             'AO_just_stabi.py' file"""
             delete_log()
             button7.place_forget()
+            status_label_on.place_forget()
+            status_label_camview.place_forget()
+            status_label_reset.place_forget()
+            status_label_off.place(x=74, y=180)
 
-        button7 = tk.Button(text='Stop', width=275, height=275,
-                     fg="red",  highlightthickness=0, bd=0,
-                     command=lambda: stop_button(),
-                     image=stop_button_image)
-        button7.image = stop_button_image
+
+        # manage buttons:
+        button1 = tk.Button(text='Anleitung', width=25,
+                    height=2, command=lambda: new_window())
+        button1.place(relx=0.8, y=160, anchor="c")
+
+
+        button2 = tk.Button(text='Start Adaptive Optik',
+                    width=39, height=4,
+                    command=lambda: start_ao())
+        button2.place(x=61, y=390, anchor="sw")
+
+
+        button3 = tk.Button(text='Reset', width=25, height=2,
+                    command=lambda: reset())
+        button3.place(relx=0.8, y=240, anchor="c")
+
+
+        button4 = tk.Button(text='Sicht der Kamera', width=25,
+                    height=2, command=lambda: cam_view())
+        button4.place(relx=0.8, y=320, anchor="c")
+
+
+        # 3 hidden buttons (5, 8, 9):
+        button5 = tk.Button(text ='', width=158, height=78,
+                    highlightthickness=0, bd=0,
+                    command=lambda: hidden_button(1),
+                    image=hidden_button_background_image)
+        button5.image = hidden_button_background_image
+        button5.place(relx=1, y=20, anchor="c")
+
+        button8 = tk.Button(text='', width=158, height=78,
+                    highlightthickness=0, bd=0,
+                    command=lambda: hidden_button(2),
+                    image=hidden_button_background_image)
+        button8.image = hidden_button_background_image
+        button8.place(relx=1, y=460, anchor="c")
+
+        button9 = tk.Button(text='', width=158, height=78,
+                    highlightthickness=0, bd=0,
+                    command=lambda: hidden_button(3),
+                    image=hidden_button_background_image)
+        button9.image = hidden_button_background_image
+        button9.place(relx=0, y=460, anchor="c")
+
+
+        # Stop Button:
+        button7 = tk.Button(text='Stop', width=39, height=4,
+                     fg="red", command=lambda: stop_button())
+
+
+
+        # status label = ON:
+        status_label_on = tk.Label(master, image=status_image_on)
+        status_label_on.image = status_image_on
+        # status label = OFF:
+        status_label_off = tk.Label(master, image=status_image_off)
+        status_label_off.image = status_image_off
+        # status label cam_view = ON:
+        status_label_camview = tk.Label(master, image=status_image_camview)
+        status_label_camview.image = status_image_camview
+        # status label reset = ON:
+        status_label_reset = tk.Label(master, image=status_image_reset)
+        status_label_reset.image = status_image_reset
+
+
+        # flexible close button, uncomment if needed:
+        closebutton = tk.Button(text='Close', width=25, height=2,
+                    command=lambda: master.quit())
+        # closebutton.place(relx=0.6, rely=0.9, anchor="c")
 
 
 class Instructions:
     def __init__(self, master):
         self.master = master
-        master.geometry("400x400+370-110")
+        master.geometry("400x400+405-110")
         # set background image:
         background_image = tk.PhotoImage(file=
                     "/home/pi/anleitung.gif")
